2c817ee70048ef0de855d275b29731ce072070e8
milindatgithub <mailtomilind@gmail.com>
2023-10-05 17:20:41 +0530

    ./com/yodlee/Citibank.java
    Made Changes

--- com/yodlee/Citibank.java
+++ com/yodlee/Citibank.java
@@ -3,9 +3,9 @@ package com.yodlee;
 public class Citibank{
 
 	CURRENT_TIME = "Thu Oct  5 09:27:36 IST 2023"
-	public static String RCSID = "May1"
+	public static String RCSID = "Thu Oct  5 17:01:17 IST 2023"
 	private int a=10;
-	private int d=10;
+	private int efg=0;
 	com.blah.user-thing
 s	ok123wwsssssdsd1234sssssssssdsddsssdsdsdsdsdssdsdssdsdsdsssdsdsssdsdsddsdsddddddsdsdsdssdsdsdsdsdsdddddddddd
 }sd
